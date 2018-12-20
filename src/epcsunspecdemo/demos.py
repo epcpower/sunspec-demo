75d3ac165f1e71773ff639a6edad528db7f821b1
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-12-19 22:21:02 -0500

    Move some subcommands over to demos

--- src/epcsunspecdemo/demos.py
+++ src/epcsunspecdemo/demos.py
@@ -1,5 +1,8 @@
 import time
 
+import click
+import sunspec.core.client
+
 import epcsunspecdemo.utils
 
 
@@ -14,7 +17,7 @@ def update_cmd_bits(cmd_bits, device):
     print(device.inverter.StVnd)
 
 
-def gridtied(device, invert_enable, cycles):
+def gridtied_demo(device, invert_enable, cycles):
     cmd_bits = epcsunspecdemo.utils.Flags(
         model=device.epc_control,
         point='CmdBits',
@@ -84,7 +87,7 @@ def gridtied(device, invert_enable, cycles):
         print("controlset")
 
 
-def dcdc(device, invert_enable, cycles):
+def dcdc_demo(device, invert_enable, cycles):
     cmd_bits = epcsunspecdemo.utils.Flags(
         model=device.epc_control,
         point='CmdBits',
@@ -174,3 +177,102 @@ def dcdc(device, invert_enable, cycles):
         device.epc_control.model.points['CtlSrc'].write()
 
         print("controlset")
+
+
+@click.group()
+@click.pass_obj
+def gridtied(config):
+    config.common = gridtied_demo
+
+
+@click.group()
+@click.pass_obj
+def dcdc(config):
+    config.common = dcdc_demo
+
+
+models_option = click.option(
+    '--models',
+    type=click.Path(file_okay=False),
+    default='models',
+)
+invert_enable_option = click.option('--invert-enable/--no-invert-enable')
+slave_id_option = click.option('--slave-id', type=int, default=1)
+max_count_option = click.option('--max-count', type=int, default=100)
+cycles_option = click.option('--cycles', type=int, default=25)
+
+
+@click.command()
+@click.option('--port', required=True)
+@click.option('--baudrate', type=int, default=9600)
+@models_option
+@invert_enable_option
+@slave_id_option
+@max_count_option
+@cycles_option
+@click.pass_obj
+def serial(
+        config,
+        port,
+        models,
+        invert_enable,
+        slave_id,
+        max_count,
+        baudrate,
+        cycles,
+):
+    with epcsunspecdemo.utils.fresh_smdx_path(models):
+        device = sunspec.core.client.SunSpecClientDevice(
+            slave_id=slave_id,
+            max_count=max_count,
+            device_type=sunspec.core.client.RTU,
+            name=port,
+            baudrate=baudrate,
+        )
+
+    config.common(
+        device=device,
+        invert_enable=invert_enable,
+        cycles=cycles,
+    )
+
+
+@click.command()
+@click.option('--ip')
+@models_option
+@invert_enable_option
+@slave_id_option
+@max_count_option
+@cycles_option
+@click.pass_obj
+def tcp(
+        config,
+        ip,
+        models,
+        invert_enable,
+        slave_id,
+        max_count,
+        cycles,
+):
+    with epcsunspecdemo.utils.fresh_smdx_path(models):
+        device = sunspec.core.client.SunSpecClientDevice(
+            slave_id=slave_id,
+            max_count=max_count,
+            device_type=sunspec.core.client.TCP,
+            ipaddr=ip,
+        )
+
+    config.common(
+        device=device,
+        invert_enable=invert_enable,
+        cycles=cycles,
+    )
+
+
+def add_commands(group):
+    group.add_command(gridtied, name='gridtied')
+    group.add_command(dcdc, name='dcdc')
+
+    for group in (gridtied, dcdc):
+        group.add_command(serial, name='serial')
+        group.add_command(tcp, name='tcp')
