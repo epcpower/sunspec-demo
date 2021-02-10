1887f5c8b9ac8bc93cdce7b0c8f083b21d62dc5e
Gordon Schmidt <gordon.schmidt@epcpower.com>
2021-02-09 16:54:11 -0800

    Add version and click option

--- setup.py
+++ setup.py
@@ -7,8 +7,18 @@ def read(fname):
         return file.read()
 
 
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name='epcsunspecdemo',
+    version=get_version('./src/epcsunspecdemo/_version.py'),
     long_description=read('README.rst'),
     long_description_content_type='text/x-rst',
     packages=setuptools.find_packages('src'),
