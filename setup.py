dd760fab37f01a3974fd8556a5ffc11589008805
Gordon Schmidt <gordon.schmidt@epcpower.com>
2021-02-10 17:27:35 -0800

    build: add setup.cfg

--- setup.py
+++ setup.py
@@ -1,46 +1,7 @@
-import os
+import pathlib
 import setuptools
 
 
-def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as file:
-        return file.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-
 setuptools.setup(
-    name='epcsunspecdemo',
-    version=get_version('./src/epcsunspecdemo/_version.py'),
-    long_description=read('README.rst'),
-    long_description_content_type='text/x-rst',
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    install_requires=[
-        'attrs',
-        'click',
-        'pysunspec',
-        'toolz',
-        'tqdm',
-    ],
-    extras_require={
-        'development': [
-            'gitignoreio',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            (
-                'epcsunspecdemo = epcsunspecdemo.cli:cli',
-            ),
-        ],
-    },
+    long_description=pathlib.Path("README.rst").read_text(encoding="utf8"),
 )
--- src/epcsunspecdemo/_version.py
+++ src/epcsunspecdemo/_version.py
@@ -1,3 +1,4 @@
-# This file is imported from __init__.py and exec'd from setup.py
+# This file is imported from '__init__.py'.
+# The __version__ value must be updated here as part of the release process.
 
 __version__ = "0.0.0+dev"
