12ed497ed1c2cb072af1936cd1beb246cddc0d33
Alex Anker <alex.anker@epcpower.com>
2022-01-19 13:53:06 -0800

    manual poetry versioning in ci

--- src/epcsunspecdemo/__init__.py
+++ src/epcsunspecdemo/__init__.py
@@ -1,10 +1,1 @@
-import toml
-import os
-import subprocess
-"""Top-level package for epcsunspecdemo."""
-
-
-#get version from toml
-parent_dir = os.path.realpath(".")
-toml_dict = toml.load(parent_dir + '/pyproject.toml')
-__version__ = toml_dict['tool']['poetry']['version']
+__version__ = "0.0.0"
