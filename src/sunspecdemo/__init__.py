29c07b0073c8fa870accf2cbcc0ca0d7ceba8560
alex anker <alex.anker@epcpower.com>
2022-04-12 14:40:37 -0700

    update to keep poetry consistent

--- src/sunspecdemo/__init__.py
+++ src/sunspecdemo/__init__.py
@@ -1,3 +1,3 @@
 # For development and git commit, the __version__ is set to the build placeholder 0.0.0
-# For release, the __version__ is modified by poetry dynamic versioning with the actual released version
+# For release, the __version__ is modified by poetry dynamic versioning with the GitHub tagged version
 __version__ = "0.0.0"
