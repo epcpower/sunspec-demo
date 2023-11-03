da4c9ab79587bd72039d8d165ba4b1fe3aca9da9
Fabian Ezeigwe <fabian.ezeigwe@epcpower.com>
2023-11-03 09:35:56 -0700

    Fixing PR issues 'poetry-dynamic-versioning'

--- src/sunspecdemo/__init__.py
+++ src/sunspecdemo/__init__.py
@@ -3,3 +3,6 @@
 # For release/distribution, the __version__ variable below is modified
 # during CI by poetry dynamic versioning with the github tagged version.
 __version__ = "0.0.0"
+
+__sha__ = get_git_revision_hash()
+__version_tag__ = "v{}".format(__version__)
